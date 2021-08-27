7a55eeb7190dd1c82d8e7fa50ed74cde2a0b912b
hegnemurd <hegne.murd@gmail.com>
2021-08-27 11:43:41 +0100

    Running unit and end-to-end tests - creating the file and creating basic tests to run

---
+++ developing-faster-with-the-vue-cli-by-john-papa/running-unit-and-end-to-end-tests/vue-test-jest/src/components/HiChild.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Child says {{ message }}</h2>
+    <div v-if="error">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    error() {
+      return this.message.trim().length < 3
+        ? "The child can say bigger words than that!"
+        : "";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
